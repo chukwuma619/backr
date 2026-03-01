4a699922f12dbf754547e51741bebf9bba2d7d16
Chukwuma Ebube <chukwuma619@gmail.com>
2026-03-01 23:57:45 +0100

    add shadcn colors and all its components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +31,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TooltipProvider>
         {children}
+        </TooltipProvider>
+        <Toaster />
+
       </body>
     </html>
   );
